b07aab63d37f30634bd92da98b1ee407df4cacec
Gleydson Tavares <tavares.gleydson@gmail.com>
2018-08-17 18:17:55 -0300

    add generator to tests

--- spec/jsonapi_swagger_blocks_generator_spec.rb
+++ spec/jsonapi_swagger_blocks_generator_spec.rb
@@ -1,48 +1,31 @@
 # frozen_string_literal: true
 
 require_relative "./support/movie_context.rb"
+require "rails/generators/testing/behaviour"
+require "generators/jsonapi/swagger_blocks_generator"
+
 
 RSpec.describe Jsonapi::Swagger::Blocks::Generator do
   include_context "movie class"
+  include FileUtils
 
-  context "when testing model template" do
-    it "return correct hash when seriazable is called" do
+  let(:generator) { Jsonapi::Generators::SwaggerBlocksGenerator}
 
-      serializable_hash = JSONAPI::ResourceSerializer.new(MovieResource, include: ["actors"]).serialize_to_hash(MovieResource.new(movie, nil))
-      expect(serializable_hash[:data].length).to eq 5
-      expect(serializable_hash[:data]["relationships"].length).to eq 4
-      expect(serializable_hash[:data]["attributes"].length).to eq 2
-      expect(serializable_hash[:data]["links"]).to be_instance_of(Hash)
-
-      expect(serializable_hash[:included]).to be_instance_of(Array)
-      expect(serializable_hash[:included][0]).to be_instance_of(Hash)
-      expect(serializable_hash[:included].length).to eq 3
-
-      serializable_hash = JSONAPI::ResourceSerializer.new(MovieResource).serialize_to_hash(MovieResource.new(movie, nil))
-
-      expect(serializable_hash[:data]).to be_instance_of(Hash)
-      expect(serializable_hash[:included]).to be nil
-    end
+  before do
+    generator.start(["Movie"])
   end
 
-  it "returns correct nested includes when serializable_hash is called" do
-    # 3 actors, 3 agencies
-    include_object_total = 6
+  # after do
+  #   rm_rf File.join("app")
+  # end
 
-    serializable_hash = JSONAPI::ResourceSerializer.new(MovieResource, include: ["actors", "actors.agency"]).serialize_to_hash(MovieResource.new(movie, nil))
+  context "when testing movie resource" do
+    it "return correct model test file" do
 
-    expect(serializable_hash[:included]).to be_instance_of(Array)
-    expect(serializable_hash[:included].length).to eq include_object_total
-    (0..include_object_total - 1).each do |include|
-      expect(serializable_hash[:included][include]).to be_instance_of(Hash)
     end
 
-    serializable_hash = JSONAPI::ResourceSerializer.new(MovieResource, include: ["actors.agency"]).serialize_to_hash(MovieResource.new(movie, nil))
+    it "return correct controller test file" do
 
-    expect(serializable_hash[:included]).to be_instance_of(Array)
-    expect(serializable_hash[:included].length).to eq include_object_total
-    (0..include_object_total - 1).each do |include|
-      expect(serializable_hash[:included][include]).to be_instance_of(Hash)
     end
   end
 end
